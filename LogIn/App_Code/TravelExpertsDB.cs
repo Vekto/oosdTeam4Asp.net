ab7c476ea6ed66a3528026504ab83c29f1d332da
Devin Ireland <me@monadone.com>
2016-02-04 18:47:39 -0700

    method GetCustomerNames()

--- LogIn/App_Code/TravelExpertsDB.cs
+++ LogIn/App_Code/TravelExpertsDB.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 // ReSharper disable All
 
 public static class TravelExpertsDB
@@ -9,5 +11,34 @@ public static class TravelExpertsDB
             "Data Source=localhost\\SAIT;Initial Catalog=TravelExperts;Integrated Security=True";
         SqlConnection connection = new SqlConnection(connectionString);
         return connection;
+    }
+
+    /// <summary>
+    /// Populates a list of <see cref="Customer"/> objects
+    /// </summary>
+    /// <returns>List of <see cref="Customer"/></returns>
+    public static List<Customer> GetCustomerNames()
+    {
+        var sql = @"SELECT c.CustomerId, c.CustFirstName, c.CustLastName, c.UserName FROM Customers c ORDER BY c.UserName";
+
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            using (var reader = new SqlCommand(sql, connection).ExecuteReader())
+            {
+                var customers = new List<Customer>();
+
+                while (reader.Read())
+                    customers.Add(new Customer
+                    {
+                        CustomerId = reader.GetInt32(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2),
+                        Username = reader.GetString(3)
+                    });
+
+                return customers;
+            }
+        }
     }
 }
